5113037547b5925607b5a089787331433a2a5a4e
Matthew Richards <matthewrichards234@gmail.com>
2025-01-03 16:09:34 -0500

    finished game

--- higher_lower.py
+++ higher_lower.py
@@ -2,24 +2,6 @@ import random
 
 def higher_lower():
     r = random.randint(1, 100)
-    guess = int(input("Guess a number between 1 - 100 \n"))
-    while guess != r:
-        if guess < r:
-            print("You guessed a number LOWER that the answer.")
-            guess = guess = int(input("Guess a number between 1 - 100 \n"))
-        elif guess > r:
-            print("You guessed a number HIGHER that the answer.")
-            guess = guess = int(input("Guess a number between 1 - 100 \n"))
-        else:
-            print("Correct!")
-            return True
-
-print(higher_lower())
-
-import random
-
-def higher_lower():
-    r = random.randint(1, 100)
     print("Welcome to the Higher-Lower Game!")
     guess = int(input("Guess a number between 1 - 100: \n"))
     while guess != r:
