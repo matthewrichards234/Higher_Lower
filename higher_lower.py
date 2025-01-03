06c295faaecc3c442f969c1cdef99162a8c4ec11
Matthew Richards <matthewrichards234@gmail.com>
2025-01-03 16:37:56 -0500

    clever solution!

--- higher_lower.py
+++ higher_lower.py
@@ -13,4 +13,28 @@ def higher_lower():
     print("Correct! You guessed the number.")
     return True
 
-print(higher_lower())
+#print(higher_lower())
+
+def enter_players():
+    # Store player names
+    players = []
+
+    # Number of players playing
+    n = int(input("Enter number of players (1-5): \n"))
+
+    # Base case limiting total players
+    if (n < 1) or (n > 5):
+        print("Please enter player number betwee 1-5.\n")
+
+    for p in range(n):
+        while len(players) < n:
+            p = input("Enter player name: \n").title()
+            if p in players:
+                print("Please enter unique names per player.\n")
+            else:
+                players.append(p)
+        print("All players added! Good luck!")
+
+    return players
+
+print(enter_players())
